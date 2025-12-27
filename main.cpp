a20cebf22e428131b8a2eb5ac26acaf84c701bfe
arshxbt <testimony.ashley.dev@gmail.com>
2025-12-27 10:40:06 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout <<" _ _ _ _   _ _ _ _   _"<< endl;
+    cout <<"|  _ _  | |  _ _ _| | |"<< endl;
+    cout <<"| |   | | | |_ _ _  | |"<< endl;
+    cout <<"| |_ _| | |_ _ _  | | |_ _ _"<< endl;
+    cout <<"|_ _ _  |       | | |  _ _  |"<< endl;
+    cout <<"      | |       | | | |   | |"<< endl;
+    cout <<"      |_| "<< endl;
+    cout <<""<< endl;
+    cout <<""<< endl;
+    cout <<""<< endl;
+    return 0;
+}
